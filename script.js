8fbe6e6d3cc2bd40169d1dae483e14cf288957f2
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2023-11-25 21:12:23 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// Initializing a variable 'count' to store the number of likes
 var count = 0;
-      var button = document.getElementById("button-like");
-      var countDisplay = document.getElementById("like-count");
-      button.addEventListener("click", function() {
-         count++;
-         countDisplay.innerHTML = count;
-      });
+
+// Getting a reference to the "like" button element by its ID
+var button = document.getElementById("button-like");
+
+// Getting a reference to the element that displays the like count by its ID
+var countDisplay = document.getElementById("like-count");
+
+// Adding a click event listener to the "like" button
+button.addEventListener("click", function() {
+    // Incrementing the like count when the button is clicked
+    count++;
+
+    // Updating the content of the element that displays the like count
+    countDisplay.innerHTML = count;
+});
